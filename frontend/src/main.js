664be8bb19382c2ac4aaa1600aacbfe240374d46
joecos050628kun@gmail.com <130775789+JoecosB@users.noreply.github.com>
2024-06-21 10:55:58 +0800

    Add Markdown dependencies in main.js and packages.json

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VMdEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+import Prism from 'prismjs';
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+
+VMdEditor.use(githubTheme, {
+  Prism,
+});
+
+const app = createApp(App);
+app.use(VMdEditor);
+app.mount('#app');
