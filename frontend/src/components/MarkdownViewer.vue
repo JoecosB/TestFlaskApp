64776d16093fef3e96ced6853839a5b2d0422fe2
joecos050628kun@gmail.com <130775789+JoecosB@users.noreply.github.com>
2024-08-21 11:00:56 +0800

    Add line numbers for code blocks.

--- frontend/src/components/MarkdownViewer.vue
+++ frontend/src/components/MarkdownViewer.vue
@@ -26,6 +26,374 @@ export default {
   },
   setup(props, { emit }) {
     const fileContent = ref('');
+    window.hljs = hljs;
+
+    (function (w, d) {
+    'use strict';
+    console.log(w)
+
+    var TABLE_NAME = 'hljs-ln',
+        LINE_NAME = 'hljs-ln-line',
+        CODE_BLOCK_NAME = 'hljs-ln-code',
+        NUMBERS_BLOCK_NAME = 'hljs-ln-numbers',
+        NUMBER_LINE_NAME = 'hljs-ln-n',
+        DATA_ATTR_NAME = 'data-line-number',
+        BREAK_LINE_REGEXP = /\r\n|\r|\n/g;
+
+    if (w.hljs) {
+        w.hljs.initLineNumbersOnLoad = initLineNumbersOnLoad;
+        w.hljs.lineNumbersBlock = lineNumbersBlock;
+        w.hljs.lineNumbersValue = lineNumbersValue;
+
+        addStyles();
+    } else {
+        w.console.error('highlight.js not detected!');
+    }
+
+    function isHljsLnCodeDescendant(domElt) {
+        var curElt = domElt;
+        while (curElt) {
+            if (curElt.className && curElt.className.indexOf('hljs-ln-code') !== -1) {
+                return true;
+            }
+            curElt = curElt.parentNode;
+        }
+        return false;
+    }
+
+    function getHljsLnTable(hljsLnDomElt) {
+        var curElt = hljsLnDomElt;
+        while (curElt.nodeName !== 'TABLE') {
+            curElt = curElt.parentNode;
+        }
+        return curElt;
+    }
+
+    // Function to workaround a copy issue with Microsoft Edge.
+    // Due to hljs-ln wrapping the lines of code inside a <table> element,
+    // itself wrapped inside a <pre> element, window.getSelection().toString()
+    // does not contain any line breaks. So we need to get them back using the
+    // rendered code in the DOM as reference.
+    function edgeGetSelectedCodeLines(selection) {
+        // current selected text without line breaks
+        var selectionText = selection.toString();
+
+        // get the <td> element wrapping the first line of selected code
+        var tdAnchor = selection.anchorNode;
+        while (tdAnchor.nodeName !== 'TD') {
+            tdAnchor = tdAnchor.parentNode;
+        }
+
+        // get the <td> element wrapping the last line of selected code
+        var tdFocus = selection.focusNode;
+        while (tdFocus.nodeName !== 'TD') {
+            tdFocus = tdFocus.parentNode;
+        }
+
+        // extract line numbers
+        var firstLineNumber = parseInt(tdAnchor.dataset.lineNumber);
+        var lastLineNumber = parseInt(tdFocus.dataset.lineNumber);
+
+        // multi-lines copied case
+        if (firstLineNumber != lastLineNumber) {
+
+            var firstLineText = tdAnchor.textContent;
+            var lastLineText = tdFocus.textContent;
+
+            // if the selection was made backward, swap values
+            if (firstLineNumber > lastLineNumber) {
+                var tmp = firstLineNumber;
+                firstLineNumber = lastLineNumber;
+                lastLineNumber = tmp;
+                tmp = firstLineText;
+                firstLineText = lastLineText;
+                lastLineText = tmp;
+            }
+
+            // discard not copied characters in first line
+            while (selectionText.indexOf(firstLineText) !== 0) {
+                firstLineText = firstLineText.slice(1);
+            }
+
+            // discard not copied characters in last line
+            while (selectionText.lastIndexOf(lastLineText) === -1) {
+                lastLineText = lastLineText.slice(0, -1);
+            }
+
+            // reconstruct and return the real copied text
+            var selectedText = firstLineText;
+            var hljsLnTable = getHljsLnTable(tdAnchor);
+            for (var i = firstLineNumber + 1 ; i < lastLineNumber ; ++i) {
+                var codeLineSel = format('.{0}[{1}="{2}"]', [CODE_BLOCK_NAME, DATA_ATTR_NAME, i]);
+                var codeLineElt = hljsLnTable.querySelector(codeLineSel);
+                selectedText += '\n' + codeLineElt.textContent;
+            }
+            selectedText += '\n' + lastLineText;
+            return selectedText;
+        // single copied line case
+        } else {
+            return selectionText;
+        }
+    }
+
+    // ensure consistent code copy/paste behavior across all browsers
+    // (see https://github.com/wcoder/highlightjs-line-numbers.js/issues/51)
+    document.addEventListener('copy', function(e) {
+        // get current selection
+        var selection = window.getSelection();
+        // override behavior when one wants to copy line of codes
+        if (isHljsLnCodeDescendant(selection.anchorNode)) {
+            var selectionText;
+            // workaround an issue with Microsoft Edge as copied line breaks
+            // are removed otherwise from the selection string
+            if (window.navigator.userAgent.indexOf('Edge') !== -1) {
+                selectionText = edgeGetSelectedCodeLines(selection);
+            } else {
+                // other browsers can directly use the selection string
+                selectionText = selection.toString();
+            }
+            e.clipboardData.setData('text/plain', selectionText);
+            e.preventDefault();
+        }
+    });
+
+    function addStyles () {
+        var css = d.createElement('style');
+        css.type = 'text/css';
+        css.innerHTML = format(
+            '.{0}{border-collapse:collapse}' +
+            '.{0} td{padding-top:0}' +
+            '.{0} td{padding-bottom:0}' +
+            '.{1}:before{content:attr({2})}',
+        [
+            TABLE_NAME,
+            NUMBER_LINE_NAME,
+            DATA_ATTR_NAME
+        ]);
+        d.getElementsByTagName('head')[0].appendChild(css);
+    }
+
+    function initLineNumbersOnLoad (options) {
+        if (d.readyState === 'interactive' || d.readyState === 'complete') {
+            documentReady(options);
+        } else {
+            w.addEventListener('DOMContentLoaded', function () {
+                documentReady(options);
+            });
+        }
+    }
+
+    function documentReady (options) {
+        try {
+            var blocks = d.querySelectorAll('code.hljs,code.nohighlight');
+
+            for (var i in blocks) {
+                if (blocks.hasOwnProperty(i)) {
+                    if (!isPluginDisabledForBlock(blocks[i])) {
+                        lineNumbersBlock(blocks[i], options);
+                    }
+                }
+            }
+        } catch (e) {
+            w.console.error('LineNumbers error: ', e);
+        }
+    }
+
+    function isPluginDisabledForBlock(element) {
+        return element.classList.contains('nohljsln');
+    }
+
+    function lineNumbersBlock (element, options) {
+        if (typeof element !== 'object') return;
+
+        async(function () {
+            element.innerHTML = lineNumbersInternal(element, options);
+        });
+    }
+
+    function lineNumbersValue (value, options) {
+        if (typeof value !== 'string') return;
+
+        var element = document.createElement('code')
+        element.innerHTML = value
+
+        return lineNumbersInternal(element, options);
+    }
+
+    function lineNumbersInternal (element, options) {
+
+        var internalOptions = mapOptions(element, options);
+
+        duplicateMultilineNodes(element);
+
+        return addLineNumbersBlockFor(element.innerHTML, internalOptions);
+    }
+
+    function addLineNumbersBlockFor (inputHtml, options) {
+        var lines = getLines(inputHtml);
+
+        // if last line contains only carriage return remove it
+        if (lines[lines.length-1].trim() === '') {
+            lines.pop();
+        }
+
+        if (lines.length > 1 || options.singleLine) {
+            var html = '';
+
+            for (var i = 0, l = lines.length; i < l; i++) {
+                html += format(
+                    '<tr>' +
+                        '<td class="{0} {1}" {3}="{5}">' +
+                            '<div class="{2}" {3}="{5}"></div>' +
+                        '</td>' +
+                        '<td class="{0} {4}" {3}="{5}">' +
+                            '{6}' +
+                        '</td>' +
+                    '</tr>',
+                [
+                    LINE_NAME,
+                    NUMBERS_BLOCK_NAME,
+                    NUMBER_LINE_NAME,
+                    DATA_ATTR_NAME,
+                    CODE_BLOCK_NAME,
+                    i + options.startFrom,
+                    lines[i].length > 0 ? lines[i] : ' '
+                ]);
+            }
+
+            return format('<table class="{0}">{1}</table>', [ TABLE_NAME, html ]);
+        }
+
+        return inputHtml;
+    }
+
+    /**
+     * @param {HTMLElement} element Code block.
+     * @param {Object} options External API options.
+     * @returns {Object} Internal API options.
+     */
+    function mapOptions (element, options) {
+        options = options || {};
+        return {
+            singleLine: getSingleLineOption(options),
+            startFrom: getStartFromOption(element, options)
+        };
+    }
+
+    function getSingleLineOption (options) {
+        var defaultValue = false;
+        if (!!options.singleLine) {
+            return options.singleLine;
+        }
+        return defaultValue;
+    }
+
+    function getStartFromOption (element, options) {
+        var defaultValue = 1;
+        var startFrom = defaultValue;
+
+        if (isFinite(options.startFrom)) {
+            startFrom = options.startFrom;
+        }
+
+        // can be overridden because local option is priority
+        var value = getAttribute(element, 'data-ln-start-from');
+        if (value !== null) {
+            startFrom = toNumber(value, defaultValue);
+        }
+
+        return startFrom;
+    }
+
+    /**
+     * Recursive method for fix multi-line elements implementation in highlight.js
+     * Doing deep passage on child nodes.
+     * @param {HTMLElement} element
+     */
+    function duplicateMultilineNodes (element) {
+        var nodes = element.childNodes;
+        for (var node in nodes) {
+            if (nodes.hasOwnProperty(node)) {
+                var child = nodes[node];
+                if (getLinesCount(child.textContent) > 0) {
+                    if (child.childNodes.length > 0) {
+                        duplicateMultilineNodes(child);
+                    } else {
+                        duplicateMultilineNode(child.parentNode);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Method for fix multi-line elements implementation in highlight.js
+     * @param {HTMLElement} element
+     */
+    function duplicateMultilineNode (element) {
+        var className = element.className;
+
+        if ( ! /hljs-/.test(className)) return;
+
+        var lines = getLines(element.innerHTML);
+
+        for (var i = 0, result = ''; i < lines.length; i++) {
+            var lineText = lines[i].length > 0 ? lines[i] : ' ';
+            result += format('<span class="{0}">{1}</span>\n', [ className,  lineText ]);
+        }
+
+        element.innerHTML = result.trim();
+    }
+
+    function getLines (text) {
+        if (text.length === 0) return [];
+        return text.split(BREAK_LINE_REGEXP);
+    }
+
+    function getLinesCount (text) {
+        return (text.trim().match(BREAK_LINE_REGEXP) || []).length;
+    }
+
+    ///
+    /// HELPERS
+    ///
+
+    function async (func) {
+        w.setTimeout(func, 0);
+    }
+
+    /**
+     * {@link https://wcoder.github.io/notes/string-format-for-string-formating-in-javascript}
+     * @param {string} format
+     * @param {array} args
+     */
+    function format (format, args) {
+        return format.replace(/\{(\d+)\}/g, function(m, n){
+            return args[n] !== undefined ? args[n] : m;
+        });
+    }
+
+    /**
+     * @param {HTMLElement} element Code block.
+     * @param {String} attrName Attribute name.
+     * @returns {String} Attribute value or empty.
+     */
+    function getAttribute (element, attrName) {
+        return element.hasAttribute(attrName) ? element.getAttribute(attrName) : null;
+    }
+
+    /**
+     * @param {String} str Source string.
+     * @param {Number} fallback Fallback value.
+     * @returns Parsed number or fallback value.
+     */
+    function toNumber (str, fallback) {
+        if (!str) return fallback;
+        var number = Number(str);
+        return isFinite(number) ? number : fallback;
+    }
+
+    }(window, document));
 
     const fetchTextFile = async () => {
       try {
@@ -34,6 +402,7 @@ export default {
         fileContent.value = response.data;
         await nextTick();
         addCopyButtons();
+        addLineNumbers()
       } catch (error) {
         console.error('Error fetching text file:', error);
       } finally {
@@ -41,6 +410,7 @@ export default {
       }
     };
 
+    // 建立函数，在代码块右上角添加复制按钮
     const addCopyButtons = () => {
       document.querySelectorAll('pre').forEach((pre) => {
         const button = document.createElement('button');
@@ -60,16 +430,26 @@ export default {
       });
     };
 
+    //建立函数，在代码块左方添加行号
+    const addLineNumbers = () => {
+      document.querySelectorAll('pre code.hljs').forEach((block) => {
+        window.hljs.lineNumbersBlock(block, { singleLine: true });
+      });
+    };
+
+    // 当文件名变化时重新获取文件
     watch(() => props.filename, (newFilename) => {
       if (newFilename) {
         fetchTextFile();
       }
     });
 
+    // 组件挂载后获取文件
     onMounted(() => {
       fetchTextFile();
     });
 
+    // 建立markdown解释器
     const md = new MarkdownIt({
       linkify: true,
       highlight: function (str, lang) {
@@ -98,6 +478,7 @@ export default {
       return `<img src="${src}" alt="${alt}" style="max-width:${width}; height:${height};" />`;
     };
 
+    // 将markdown渲文本渲染html
     const result = computed(() => {
       return md.render(fileContent.value);
     });
@@ -111,18 +492,16 @@ export default {
 </script>
 
 <style>
-/* 针对代码块的自定义样式 */
 pre code.hljs {
   background-color: #f0f0f0; /* 灰色背景 */
-  border: 1px solid #ccc;    /* 边框颜色 */
-  padding: 10px;             /* 内边距 */
+  border: none;
+  padding-left: 10px;             /* 内边距 */
   border-radius: 5px;        /* 圆角 */
   display: block;
   overflow-x: auto;          /* 横向滚动条 */
   white-space: pre-wrap;     /* 保持换行 */
 }
 
-/* 针对内联代码块的样式 */
 code.hljs {
   background-color: #f0f0f0;
   border: 1px solid #ccc;
@@ -130,7 +509,6 @@ code.hljs {
   border-radius: 3px;
 }
 
-/* 针对代码块的自定义样式 */
 pre code {
   background-color: #f0f0f0;
   border: none;
@@ -165,5 +543,21 @@ code {
 .copy-button:hover {
   background-color: #333; /* 鼠标悬停时的颜色 */
   color: #ffffff
+}
+
+/* 行号容器的基础样式 */
+.hljs-ln-numbers {
+  text-align: right;         /* 行号右对齐 */
+  padding-right: 10px;       /* 行号与代码之间的间距 */
+  vertical-align: top;       /* 行号垂直对齐 */
+  user-select: none;         /* 禁止用户选择行号 */
+  color: #999;               /* 行号的颜色 */
+  background-color: #f0f0f0; /* 行号的背景色 */
+  border-right: 1px solid #ddd; /* 行号与代码之间的分割线 */
+}
+
+/* 代码容器的基础样式 */
+.hljs-ln-code {
+  padding-left: 10px;        /* 代码与行号之间的间距 */
 }
 </style>
