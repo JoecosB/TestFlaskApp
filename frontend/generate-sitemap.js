8202b832e43de1c1171f9da3bd069983a5eb00ad
joecos050628kun@gmail.com <130775789+JoecosB@users.noreply.github.com>
2024-09-14 18:04:44 +0800

    Fix sitemap generation

--- frontend/generate-sitemap.js
+++ frontend/generate-sitemap.js
@@ -5,12 +5,12 @@ const routes = [
    '/',
    '/blog',
    '/friendlink',
-   '/blog/Docker&#x5B66;&#x4E60;&#x7B14;&#x8BB0;(2)',
-   '/blog/Docker&#x5B66;&#x4E60;&#x7B14;&#x8BB0;(1)',
-    '/blog/&#x4F7F;&#x7528;markdown-it&#x6E32;&#x67D3;markdown&#x6587;&#x672C;',
-    '/blog/&#x4F7F;&#x7528;highlightjs-line-numbers.js&#x4E3A;markdown&#x6E32;&#x67D3;&#x63D0;&#x4F9B;&#x884C;&#x53F7;',
-    '/blog/&#x4F7F;&#x7528;ssh&#x5728;&#x5916;&#x7F51;&#x8FDE;&#x63A5;&#x5C40;&#x57DF;&#x7F51;&#x5185;&#x7684;&#x6811;&#x8393;&#x6D3E;',
-    '/blog/&#x5982;&#x4F55;&#x7ED9;&#x534E;&#x7855;&#x8DEF;&#x7531;&#x5668;&#x5237;&#x6885;&#x6797;&#x56FA;&#x4EF6;',
+   '/blog/Docker学习笔记(2)',
+   '/blog/Docker学习笔记(1)',
+    '/blog/使用markdown-it渲染markdown文本',
+    '/blog/使用highlightjs-line-numbers.js为markdown渲染提供行号',
+    '/blog/使用ssh在外网连接局域网内的树莓派',
+    '/blog/如何给华硕路由器刷梅林固件',
 ];
 
 async function generateSitemap() {
